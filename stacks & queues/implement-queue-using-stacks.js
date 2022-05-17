5b23479a28f377d6d2290f9218ad7f08523f1b49
Claudia-teng <sydeng102@gmail.com>
2022-05-17 17:14:22 +0800

    0517 practice

--- stacks & queues/implement-queue-using-stacks.js
+++ stacks & queues/implement-queue-using-stacks.js
@@ -1,5 +1,5 @@
 // Only use the methods that stacks have
-// pop / push / peek(last item)
+// pop / push / peek (last item)
 
 var MyQueue = function() {
   this.s1 = [];
@@ -19,7 +19,7 @@ MyQueue.prototype.push = function(x) {
   this.s1.push(x);
 };
 
-// queue pop -> remove first item
+// queue pop -> remove the first item
 // reverse the order in s2 to get the bottom (first) item
 // note: do not need to put back items to s1
 // because next time we can access the first item just using s2
@@ -49,4 +49,49 @@ MyQueue.prototype.peek = function() {
 
 MyQueue.prototype.empty = function() {
   return this.s1.length === 0 && this.s2.length === 0;
+};
+
+
+// Udemy (more complex)
+
+var MyQueue = function() {
+  this.forward = [];
+  this.backward = [];
+};
+
+
+// queue push -> add to the end
+// always do this in forward stack
+
+MyQueue.prototype.push = function(x) {
+  while (this.backward.length !== 0) {
+    this.forward.push(this.backward.pop());
+  }
+  this.forward.push(x);
+};
+
+// queue push -> remove the first item
+// always do this in backward stack
+
+MyQueue.prototype.pop = function(x) {
+  while (this.forward.length !== 0) {
+    this.backward.push(this.forward.pop());
+  }
+  return this.backward.pop();
+};
+
+// queue peek -> get the first item
+// always do this in backward stack
+
+MyQueue.prototype.peek = function() {
+  while (this.forward.length !== 0) {
+    this.backward.push(this.forward.pop());
+  }
+  return this.backward[this.backward.length-1];
+};
+
+// queue empty -> check if two stacks are all empty
+
+MyQueue.prototype.empty = function() {
+  return this.forward.length === 0 && this.backward.length === 0;
 };
