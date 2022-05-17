e818eda4b4ac0115ccd40426aabbf077a5c60bde
Claudia-teng <sydeng102@gmail.com>
2022-05-17 13:44:49 +0800

    0517 practice

---
+++ array/two-sum.js
@@ -0,0 +1,56 @@
+// version 1
+// brute force
+// O(n^2) runtime complexity
+
+var twoSum = function(nums, target) {
+  for (let i=0; i<nums.length-1; i++) {
+    for (let j=i+1; j<nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+// version 2
+// use hash table to save key(num): value(index)
+// check if goal num is in the hash map
+// & check the goal num is itself (by index) (can not use a num twice)
+// O(n) runtime complexity
+
+var twoSum = function(nums, target) {
+  let myMap = new Map()
+  for (let i=0; i<nums.length; i++) {
+    myMap.set(nums[i], i);
+  }
+  for (let i=0; i<nums.length; i++) {
+    let goal = target - nums[i];
+    if (myMap.has(goal) && myMap.get(goal) !== i) {
+      return [myMap.get(goal), i];
+    }
+  }
+}
+
+// Note:
+// I use let ht = {} first to create a hash table
+// but have problems with saving index 0 (which will turn ht[nums[i]] to false)
+// if i save the index with string type
+// it'll have problems comparing i index (type number) & ht[nums[i]] (type string) with !== strict equal
+
+// version 3
+// check if the goal num is in the hash table
+// no -> save in hash table key(num): value(index)
+// yes -> return indices
+// O(n) runtime complexity
+
+var twoSum = function(nums, target) {
+  let ht = {};
+  for (let i=0; i<nums.length; i++) {
+    let goal = target-nums[i];
+    if (!ht[goal]) {
+      ht[nums[i]] = String(i);
+    } else {
+      return [ht[goal], i];
+    }
+  }
+}
---
+++ string/reverse-string.js
@@ -0,0 +1,18 @@
+// version 1
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+
+var reverseString = function(s) {
+  s.reverse();
+};
+
+// version 2
+
+var reverseString = function(s) {
+  let time = Math.floor(s.length/2);
+  for (let i=0; i<time; i++) {
+    let hold = s[i];
+    s[i] = s[s.length-1-i];
+    s[s.length-1-i] = hold;
+  }
+};
