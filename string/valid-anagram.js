188dc725521227bbadfa20c4bc205df55d5430f1
Claudia-teng <sydeng102@gmail.com>
2022-05-21 12:28:10 +0800

    0521 practice

---
+++ string/valid-anagram.js
@@ -0,0 +1,64 @@
+// version 1
+// use hash table to save key(num): value(count)
+// need to handle handle different lengths of s and t -> check if any value is not 0
+
+var isAnagram = function(s, t) {
+  let ht = {}
+  for (let i=0; i<s.length; i++) {
+    if (!ht[s[i]]) {
+      ht[s[i]] = 1;
+    } else {
+      ht[s[i]]++;
+    }
+  }
+  
+  for (let i=0; i<t.length; i++) {
+    if (!ht[t[i]]) {
+      return false;
+    } else {
+      ht[t[i]]--;
+    }
+  }
+  
+  return Object.values(ht).find(num => num !== 0) ? false : true;
+};
+
+// version 2
+// (better) another way to handle different lengths of s and t
+
+var isAnagram = function(s, t) {
+  let ht = {}
+  if (s.length !== t.length ) return false;
+  
+  for (let i=0; i<s.length; i++) {
+    if (!ht[s[i]]) {
+      ht[s[i]] = 1;
+    } else {
+      ht[s[i]]++;
+    }
+  }
+  
+  for (let i=0; i<t.length; i++) {
+    if (!ht[t[i]]) {
+      return false;
+    } else {
+      ht[t[i]]--;
+    }
+  }
+  return true;
+};
+
+// version 3
+// sorting
+// 1. check lengths
+// 2. sort and compare
+
+var isAnagram = function(s, t) {
+  if(s.length != t.length) return false;
+
+  var s = s.split("").sort().join("");
+  var t = t.split("").sort().join("");
+
+  return s === t;
+};
+
---
+++ string/valid-palindrome.js
@@ -0,0 +1,42 @@
+// version 1
+// use regex to filter non-alphanumeric characters
+// ^ -> not
+// i -> insensitive case
+// g -> global
+
+var isPalindrome = function(s) {
+  s = s.replace(/[^a-z0-9]/ig, '').toLowerCase();
+  if (s === s.split('').reverse().join('')) {
+    return true;
+  } else {
+    return false;
+  } 
+};
+
+// version 2
+// use charCodeAt() to filter non-alphanumeric characters
+
+var isPalindrome = function (s) {
+  s = s.toLowerCase();
+  
+  let aCode = 'a'.charCodeAt();
+  let zCode = 'z'.charCodeAt();
+  let zeroCode = '0'.charCodeAt();
+  let nineCode = '9'.charCodeAt();
+
+  let newS = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s.charCodeAt(i) >= aCode && s.charCodeAt(i) <= zCode) {
+      newS += s[i];
+    } else if (s.charCodeAt(i) >= zeroCode && s.charCodeAt(i) <= nineCode) {
+      newS += s[i];
+    }
+  }
+  
+  let revNewS = newS.split("").reverse().join("");
+  if (revNewS === newS) {
+    return true;
+  } else {
+    return false;
+  }
+};
