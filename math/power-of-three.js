f347dfaf04fa54372583f5cb98cf7415b026dae4
Claudia-teng <sydeng102@gmail.com>
2022-06-04 11:59:47 +0800

    0604 practice

---
+++ math/power-of-three.js
@@ -0,0 +1,11 @@
+// version 1
+// O(log3 n) runtime complexity
+// 1 is also power of 3 (3 ** 0)
+
+var isPowerOfThree = function(n) {
+  while (n >= 3) {
+    if (n%3 !== 0) return false;
+    n = n/3;
+  }
+  return n === 1 ? true : false; 
+};
---
+++ math/roman-to-integer.js
@@ -0,0 +1,76 @@
+// version 1
+
+var romanToInt = function(s) {
+  let map = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+  }
+  
+  let ans = 0;
+  
+  for (let i=0; i<s.length; i++) {
+    switch(s[i]) {
+      case 'I':
+        if (s[i+1] === 'V' || s[i+1] === 'X') {
+          ans -= map[s[i]];
+        } else {
+          ans += map[s[i]];  
+        }
+        break;
+      case 'X':
+        if (s[i+1] === 'L' || s[i+1] === 'C') {
+          ans -= map[s[i]];
+        } else {
+          ans += map[s[i]];  
+        }
+        break;
+      case 'C':
+        if (s[i+1] === 'D' || s[i+1] === 'M') {
+          ans -= map[s[i]];
+        } else {
+          ans += map[s[i]];  
+        }
+        break;     
+      default: 
+        ans += map[s[i]]; 
+    }
+  }
+  return ans;
+};
+
+// version 2
+// cleaner way of version 1
+// find the pattern that if cur < next, ans - cur
+
+var romanToInt = function(s) {
+  let map = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+  }
+  
+  let ans = 0;
+  
+  for (let i=0; i<s.length; i++) {
+    let cur = s[i];
+    let next = s[i+1];
+    
+    if (map[cur] < map[next]) {
+      ans -= map[cur];
+    } else {
+      ans += map[cur];
+    }
+  }
+  
+  return ans;
+};
+
