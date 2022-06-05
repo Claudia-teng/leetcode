81f7995879243c39ca59a94014d293828cee97d3
Claudia-teng <sydeng102@gmail.com>
2022-06-05 13:08:13 +0800

    0605 practice

---
+++ others/hamming-distance.js
@@ -0,0 +1,19 @@
+// version 1
+
+var hammingDistance = function(x, y) {
+  let binaryX = x.toString(2);
+  let binaryY = y.toString(2);
+  let maxLen = Math.max(binaryX.length, binaryY.length);
+  let count = 0;
+  
+  binaryX = binaryX.padStart(maxLen, '0');
+  binaryY = binaryY.padStart(maxLen, '0');
+  
+  for (let i=0; i<maxLen; i++) {
+    if (binaryX[i] !== binaryY[i]) {
+      count ++;
+    }
+  }
+  
+  return count;
+};
