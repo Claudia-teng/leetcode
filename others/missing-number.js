8eac63365746678439078a236f6d2941c16a0743
Claudia-teng <sydeng102@gmail.com>
2022-06-05 22:04:37 +0800

    0605 practice

---
+++ others/missing-number.js
@@ -0,0 +1,45 @@
+// version 1
+// O(n^2) runtime complexity
+// O(1) space complexity
+var missingNumber = function(nums) {
+  for (let i=0; i<nums.length; i++) {
+    if (!nums.includes(i)) return i;
+  }
+  return nums.length;
+};
+
+// version 2
+// O(n) runtime complexity
+// O(n) space complexity
+
+var missingNumber = function(nums) {
+  // or
+  // let max = nums.length;
+  // the max value is actually nums.length
+  let max = Math.max(...nums);
+
+  let map = {};
+  for (let i=0; i<nums.length; i++) {
+    map[nums[i]] = true;
+  }
+  for (let i=0; i<=max; i++) {
+    if (!map[i]) return i;
+  }
+};
+
+// version 3
+// totalSum - actualSum is the answer
+// O(n) runtime complexity
+// O(1) space complexity
+
+var missingNumber = function(nums) {
+  let totalSum = 0;
+  for (let i=0; i<=nums.length; i++) {
+    totalSum += i;
+  }
+  let actualSum = 0
+  for (let i=0; i<nums.length; i++) {
+    actualSum += nums[i];
+  }
+  return totalSum - actualSum;
+};
---
+++ others/valid-parentheses.js
@@ -0,0 +1,37 @@
+// version 1
+// use stack
+// O(a * b) runtime complexity
+// use indexOf inside for loop
+
+var isValid = function(s) {
+  let stack = [];
+  let left = ['(', '{', '['];
+  let right = [')', '}', ']'];
+
+  for (let i=0; i<s.length; i++) {
+    let cur = s[i];
+    let index = left.indexOf(cur);
+    if (index !== -1) {
+      stack.push(right[index]);
+      continue;
+    }
+    if (right.includes(cur)) {
+      if (stack.pop() !== cur) return false;
+    }
+  }
+  return stack.length === 0;
+};
+
+// version 2
+// cleaner way of version 1
+
+var isValid = function(s) {
+  let stack = [];
+  for(let cur of s) {
+    if(cur === '(') stack.push(')')
+    else if(cur === '{') stack.push('}')
+    else if(cur === '[') stack.push(']')
+    else if(stack.length === 0 || c !== stack.pop()) return false
+  }
+  return stack.length === 0
+};
